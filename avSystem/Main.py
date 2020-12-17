d176faf1b7a50051b3f5a5f1d4109d45da626eb9
Varun Bhat <seetkipeti@gmail.com>
2020-12-17 23:55:54 +0000

    Bug fix

--- avSystem/Main.py
+++ avSystem/Main.py
@@ -105,7 +105,7 @@ class Main:
         global vehicleSpeedArray, distanceToSignalArray
         # print("length speed: ", len(vehicleSpeedArray))
         vehicleSpeedArray.append(round(speed, 2))
-        # distanceToSignalArray.append(distanceToSignalArray[len(distanceToSignalArray)-1])
+        distanceToSignalArray.append(distanceToSignalArray[len(distanceToSignalArray)-1])
         try:
             self.vehicleSpeedGui.config(text=self.currentVehicleSpeedText.format(round(speed, 2)))
         except Exception as e:
@@ -119,7 +119,7 @@ class Main:
         # handle negative distance
         global distanceToSignalArray, vehicleSpeedArray
         # print("length distance: ", len(distanceToSignalArray))
-        # vehicleSpeedArray.append(vehicleSpeedArray[len(vehicleSpeedArray)-1])
+        vehicleSpeedArray.append(vehicleSpeedArray[len(vehicleSpeedArray)-1])
         distanceToSignalArray.append(round((self.currentDistanceToSignal - distance), 2))
         try:
             self.distanceToSignalGui.config(text=self.distanceToSignalText.format(round(distance, 2)))
